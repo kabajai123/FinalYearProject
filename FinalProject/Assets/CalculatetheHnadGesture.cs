e5a73c5abd15e68dbabc9b7579857c658553806c
kabajai123 <103850170+kabajai123@users.noreply.github.com>
2023-03-08 23:48:31 +0800

    UpatingHandGesture

--- FinalProject/Assets/CalculatetheHnadGesture.cs
+++ FinalProject/Assets/CalculatetheHnadGesture.cs
@@ -7,32 +7,38 @@ public class CalculatetheHnadGesture : MonoBehaviour
 {
     public HandState _RightHandState;
     public HandState _LeftHandState;
+
     public GameObject cube;
     public GameObject cube_position;
+
+    public Pose[] _RightJointPose;
+
+    public Vector3 _RightJointPoseX = new Vector3(0, 0, 0);
+    public Vector3 Player2Pos = new Vector3(0, 0, 0);
+    public Vector3 center = new Vector3(0, 0, 0);
 
     public HandJointID handJoint;
     public HandEnum handEnum;
 
     void Update()
     {
-        _RightHandState = NRInput.Hands.GetHandState(HandEnum.RightHand);  
-        _LeftHandState = NRInput.Hands.GetHandState(HandEnum.LeftHand);
         driving();
     }
 
     public void driving()
     {
+        _RightHandState = NRInput.Hands.GetHandState(HandEnum.RightHand);
+        _LeftHandState = NRInput.Hands.GetHandState(HandEnum.LeftHand);
+
         var handState = new HandState(handEnum);
         handState.isTracked = true;
         handState.currentGesture = HandGesture.Grab;
-        if (handState.isTracked == true)
+        if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
         {
-            Debug.Log("123321123123123123");
-            while(handState.currentGesture == HandGesture.Grab)
-            {
-                Instantiate(cube, cube_position.transform.position, cube_position.transform.rotation);
-                Debug.Log("Spawning a cube");
-            }
+            //_RightJointPoseX = GameObject.FindGameObjectWithTag(HandEnum).transform.position;
+            Player2Pos = GameObject.FindGameObjectWithTag("Player2").transform.position;
+            //Instantiate(cube, cube_position.transform.position, cube_position.transform.rotation);
+            Debug.Log("Spawning a cube");
         }
     }
 }
