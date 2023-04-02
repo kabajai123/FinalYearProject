7a42d52824aea974b09a25aca043e1c3f990e32a
ITHomeworkk <wwhin1009@gmail.com>
2023-04-02 16:58:58 +0800

    itemtesting

---
+++ FinalProject/Assets/Script/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = .1f;
+    public GameObject item;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        item.GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new
+
+        Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirection * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (item.gameObject.tag == "Player")
+        {
+            Destroy(item);
+        }
+    }
+}
