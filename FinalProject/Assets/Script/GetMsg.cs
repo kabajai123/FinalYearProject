82b0a06cef7389538fd37e13d5713ce704a93e22
HappyGrape123 <103878210+HappyGrape123@users.noreply.github.com>
2023-04-03 11:38:18 +0800

    add script on ros

---
+++ FinalProject/Assets/Script/GetMsg.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.ROSTCPConnector;
+using Pos = RosMessageTypes.UnityRoboticsDemo.UnityColorMsg;
+using Map = RosMessageTypes.UnityRoboticsDemo.UnityColorMsg;
+
+public class GetMsg : MonoBehaviour
+{
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ROSConnection.GetOrCreateInstance().Subscribe<Pos>("letgo", Local);
+        ROSConnection.GetOrCreateInstance().Subscribe<Map>("mapgo", Map);
+
+    }
+    private string[] Cut;
+    void Local(Pos _msg)
+    {
+
+        Cut = _msg.msg.Split("\n");
+        string[] x = Cut[Cut.Length - 8].Split(" ");
+        string[] y = Cut[Cut.Length - 7].Split(" ");
+        string[] z = Cut[Cut.Length - 6].Split(" ");
+        string[] rx = Cut[Cut.Length - 4].Split(" ");
+        string[] ry = Cut[Cut.Length - 3].Split(" ");
+        string[] rz = Cut[Cut.Length - 2].Split(" ");
+        string[] rw = Cut[Cut.Length - 1].Split(" ");
+
+        camera.transform.position = new Vector3(float.Parse(x[x.Length - 1]), float.Parse(y[y.Length - 1]), float.Parse(z[z.Length - 1]));
+        camera.transform.rotation = new Quaternion(float.Parse(rx[rx.Length - 1]), float.Parse(ry[ry.Length - 1]), float.Parse(rz[rz.Length - 1]), float.Parse(rw[rw.Length - 1]));
+    }
+
+    void Map(Map _data)
+    {
+        Debug.Log(_data);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
