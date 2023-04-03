b95e660d1b1edab09d37c58068390afcb90ec98a
HappyGrape123 <103878210+HappyGrape123@users.noreply.github.com>
2023-04-04 01:20:44 +0800

    a
    a

--- FinalProject/Assets/Script/HandGesture/CalculatetheHandGesture.cs
+++ FinalProject/Assets/Script/HandGesture/CalculatetheHandGesture.cs
@@ -6,46 +6,39 @@ using NRKernal;
 
 public class CalculatetheHandGesture : MonoBehaviour
 {
-    private Vector3 movingPosition;
-    //private Vector3 movingRotation;
-
     public HandState _RightHandState;
     public HandState _LeftHandState;
-
-    public GameObject RingObject;
-    private GameObject _savingRing;
-    public GameObject _Camera;
-
-    public ControllerHandEnum domainHand;
-    public Pose[] _RightJointPose;
-    public Pose[] _LeftJointPose;
-
-    private float RingX;
-    private float RingY;
-    private float RingZ;
-
-    public Vector3 _RHandPosition;
-    public Vector3 _LHandPosition;
-    public Vector3 direction;
-    public Vector3 abc;
-
     public HandJointID[] handJoint;
     public HandEnum handEnum;
 
-    public bool isSpawned = false;
-    public bool abcd;
-
-    public TMP_Text AngleText;
-
-    public List<Vector3> _KeepTrackPosition;
+    public GameObject RingObject;
+    private GameObject _savingRing;
 
     private Vector3 _IgnorePartofPosition = new Vector3(0, 0, 0);
+    public Vector3 centerPoint;
+    public Vector3 _UpdateCenterPoint;
+    public Vector3 _RDirection;
+    public Vector3 _LDirection;
+
+    public float _Rangle;
+    public float _Langle;
+
+    public int listKeepTrackRCount;
+    public int listKeepTrackLCount;
+
+    public TMP_Text _RAngleText;
+    public TMP_Text _LAngleText;
+
+    public bool isSpawned = false;
+
+    public List<Vector3> _KeepTrackRPosition;
+    public List<Vector3> _KeepTrackLPosition;
+
 
     void Start()
     {
-        domainHand = NRInput.DomainHand;
-        domainHand = ControllerHandEnum.Left;
-        _KeepTrackPosition = new List<Vector3>();
+        _KeepTrackRPosition = new List<Vector3>();
+        _KeepTrackLPosition = new List<Vector3>();
     }
 
     void Update()
@@ -54,7 +47,12 @@ public class CalculatetheHandGesture : MonoBehaviour
 
         if(_RHandPosition != _IgnorePartofPosition)
         {
-            _KeepTrackPosition.Add(_RHandPosition);
+            _KeepTrackRPosition.Add(_RHandPosition);
+        }
+
+        if(_LHandPosition != _IgnorePartofPosition)
+        {
+            _KeepTrackLPosition.Add(_LHandPosition);
         }
 
         if (!isSpawned)
@@ -67,6 +65,7 @@ public class CalculatetheHandGesture : MonoBehaviour
         {
             //Cannot Detect the hand the CenterPoint will disappear
             _savingRing.SetActive(false);
+            lostHandPosition();
             Debug.Log("Active the sphere = false");
         }
         else if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
@@ -76,28 +75,28 @@ public class CalculatetheHandGesture : MonoBehaviour
             _savingRing.SetActive(true);
             Debug.Log("Active the sphere = true");
 
-            //var step = .5f * Time.deltaTime;
+            listKeepTrackRCount = _KeepTrackRPosition.Count;
+            listKeepTrackLCount = _KeepTrackLPosition.Count;
 
-            movingPosition = new Vector3(RingX, RingY, RingZ);
-            //_savingRing.transform.position = Vector3.MoveTowards(movingPosition, movingPosition, step);
-            //Debug.Log("Position: " + _savingRing.transform.position);
+            _UpdateCenterPoint = (_KeepTrackLPosition[listKeepTrackLCount - 1] + _KeepTrackRPosition[listKeepTrackLCount - 1]) / 2;
 
-            //movingRotation = new Vector3(centerXrotation, centerYrotation, centerZrotation);
-            //obj.transform.eulerAngles = Vector3.MoveTowards(movingRotation, movingRotation, step);
-            //Debug.Log("Rotation: " + _savingRing.transform.eulerAngles);
-            int listKeepTrackCount = _KeepTrackPosition.Count;
-            direction = _KeepTrackPosition[listKeepTrackCount-1];
-            angle = Vector3.Angle(abc, direction);
-            AngleText.text = "Angle: " + angle.ToString();
-            Debug.Log("The angle range: " + angle);
-            if (direction.y < 0)
-            {
-                angle = 360 - angle;
-            }
-            if (angle > maxAngle)
-            {
-                //Destroy(_savingRing);
-            }
+            _LDirection = _UpdateCenterPoint - _KeepTrackLPosition[listKeepTrackLCount - 1];
+            _RDirection = _UpdateCenterPoint - _KeepTrackRPosition[listKeepTrackRCount - 1];
+
+            _Rangle = Vector3.Angle(_RStartingPoint, _RDirection);
+            _Langle = Vector3.Angle(_LStartingPoint, _LDirection);
+
+            _LAngleText.text = "LeftAngle: " + _Langle.ToString();
+            _RAngleText.text = "RightAngle: " + _Rangle.ToString();
+            Debug.Log("The angle range: " + _Rangle);
+            //if (direction.y < 0)
+            //{
+            //    angle = 360 - angle;
+            //}
+            //if (angle > maxAngle)
+            //{
+            //    //Destroy(_savingRing);
+            //}
         }
     }
 
@@ -108,14 +107,21 @@ public class CalculatetheHandGesture : MonoBehaviour
         //if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab && _LeftHandState.isTracked == true && _LeftHandState.currentGesture == HandGesture.Grab)
         {
             isSpawned = true;
-            _savingRing = Instantiate(RingObject, _RHandPosition, Quaternion.identity);
+            _savingRing = Instantiate(RingObject, centerPoint, Quaternion.identity);
             Debug.Log("Spawning a sphere");
         }
     }
 
-    public float center; 
-    public float maxAngle;
-    public float angle;
+    public Pose[] _RightJointPose;
+    public Pose[] _LeftJointPose;
+
+    public Vector3 _RHandPosition;
+    public Vector3 _LHandPosition;
+
+    public Vector3 _RStartingPoint;
+    public Vector3 _LStartingPoint;
+
+    public bool isGettingPosition;
 
     public void driving()
     {
@@ -130,41 +136,43 @@ public class CalculatetheHandGesture : MonoBehaviour
         _RightJointPose[0] = _RightHandState.GetJointPose(handJoint[0]);
         _LeftJointPose[0] = _LeftHandState.GetJointPose(handJoint[0]);
 
-        //Center Position & Rotation follow Joint ID Position & Rotation
-        //RingX = _RightJointPose[0].position.x - 0.25f;
-        //RingY = _RightJointPose[0].position.y + 0.05f;
-        //RingZ = _RightJointPose[0].position.z;
-
-        if(_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
+        //if(_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
+        if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab && _LeftHandState.isTracked == true && _LeftHandState.currentGesture == HandGesture.Grab)
         {
             _RHandPosition = _RightJointPose[0].position;
             Debug.Log("R:Center: " + _RHandPosition);
 
-            if (!abcd)
+            _LHandPosition = _LeftJointPose[0].position;
+            Debug.Log("L:Center: " + _LHandPosition);
+
+            centerPoint = (_RHandPosition + _LHandPosition) / 2;
+            Debug.Log("RadiusPosition: " + centerPoint);
+
+            if (!isGettingPosition)
             {
                 getHandPositionFirst();
-
             }
         }
-
-
-        //_LHandPosition = _LeftJointPose[0].position;
-        //Debug.Log("L:Center: " + _LHandPosition);
-
-        //if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab && _LeftHandState.isTracked == true && _LeftHandState.currentGesture == HandGesture.Grab)
-        //{
-        //    radius = (_RHandPosition + _LHandPosition) / 2;
-        //    Debug.Log("RadiusPosition: " + radius);
-        //}
     }
 
     public void getHandPositionFirst()
     {
-        abcd = true;
-        if(_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
+        isGettingPosition = true;
+        //if(_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab)
+        if (_RightHandState.isTracked == true && _RightHandState.currentGesture == HandGesture.Grab && _LeftHandState.isTracked == true && _LeftHandState.currentGesture == HandGesture.Grab)
         {
-            abc = _RightJointPose[0].position;
-            Debug.Log("RightHandFirstPosition: " + abc);
+            _RStartingPoint = centerPoint - _RightJointPose[0].position;
+            Debug.Log("RightHandFirstPosition: " + _RStartingPoint);
+
+            _LStartingPoint = centerPoint - _LeftJointPose[0].position;
+            Debug.Log("LeftHandFirstPosition: " + _LStartingPoint);
         }
+    }
+
+    public void lostHandPosition()
+    {
+        isGettingPosition = false;
+        _Rangle = 0;
+        _Langle = 0;
     }
 }
