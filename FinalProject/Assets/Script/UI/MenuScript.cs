b1875bf0b791a2b4ac6ad9e564f36a5f7124e614
keykypa <103879471+keykypa@users.noreply.github.com>
2023-04-05 16:01:05 +0800

    Updating the MenuScript & MainMenuScene 1

--- FinalProject/Assets/Script/UI/MenuScript.cs
+++ FinalProject/Assets/Script/UI/MenuScript.cs
@@ -5,14 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public GameObject MainMenu, ConfirmStartMenu, ConfirmNewRacewayMenu;
+   
+
     public void StartButton() {
+        MainMenu.SetActive(false);
+        ConfirmStartMenu.SetActive(true);
+        //SceneManager.LoadScene("MainCarScene");
+    }
+
+    public void ConfirmStartButton()
+    {
         SceneManager.LoadScene("MainCarScene");
     }
+
+    public void CreateNewRacewayButton()
+    {
+        
+    }
+
+    public void ConfirmNewRacewayButton()
+    {
+
+    }
+
+    public void RetryCreateRacewayButton()
+    {
+
+    }
+
     public void TutorialButton() {
         SceneManager.LoadScene("TutorialScene");
     }
+
     public void ExitButton() {
         Application.Quit();
         Debug.Log("Now Exiting");
     }
+
 }
