e48047bf1b83122b5f71f1a1d66ec832da01a121
kabajai123 <103850170+kabajai123@users.noreply.github.com>
2023-04-20 00:57:01 +0800

    SoundEffect&UIUpdate

--- FinalProject/Assets/GamePlay/Player.cs
+++ FinalProject/Assets/GamePlay/Player.cs
@@ -19,37 +19,129 @@ public class Player : MonoBehaviour
     public Animator ItemUIScroll;
     public int index;
 
+    bool gethit;
+
     public TMP_Text _ScoreText;
     public float _score = 0;
     public float _addScore = 10;
+
+    public GameObject _EndGamePanel;
+    public float _scoreEnd = 0;
+    public TMP_Text _ScoreEndText;
+
+    public static int MinCount;
+    public static int SecCount;
+    public static float MiliCount;
+    public static string _Mili;
+
+    public GameObject min;
+    public GameObject sec;
+    public GameObject mili;
+
+    public GameObject _min;
+    public GameObject _sec;
+    public GameObject _mili;
+
+    public bool checkEnd = false;
+
+    public AudioClip _spin;
+    public AudioSource audioSource;
 
     void Update()
     {
         if (hasItem)
         {
             useItem();
-        }       
+        }
+
+        if(!checkEnd)
+        {
+            timer();
+        }
     }
-    bool gethit;
+
+    public void timer()
+    {
+        MiliCount += Time.deltaTime * 10;
+        _Mili = MiliCount.ToString("0");
+        mili.GetComponent<TMP_Text>().text = "" + _Mili;
+
+        if(MiliCount >= 10)
+        {
+            MiliCount = 0;
+            SecCount += 1;
+        }
+
+        if(SecCount <= 9)
+        {
+            sec.GetComponent<TMP_Text>().text = "0" + SecCount + ": ";
+        }
+        else
+        {
+            sec.GetComponent<TMP_Text>().text = "" + SecCount + ": ";
+        }
+
+        if(SecCount >= 60)
+        {
+            SecCount = 0;
+            MinCount += 1;
+        }
+
+        if(MinCount <= 9)
+        {
+            min.GetComponent<TMP_Text>().text = "0" + MinCount + ": ";
+        }
+        else
+        {
+            min.GetComponent<TMP_Text>().text = "" +  MinCount + ":";
+        }
+    }
+
     private void OnTriggerEnter(Collider other) // get hit with item
     {        
         if (other.gameObject.tag == "ItemBox")
         {
-            other.gameObject.GetComponent<Animator>().SetBool("Enlarge", true);
-            
+            other.gameObject.GetComponent<Animator>().SetBool("Enlarge", true);            
             StartCoroutine(RespawnCheck(other.gameObject));
             gethit = true;
-            if (hasItem == false) {
+
+            if (hasItem == false)
+            {
                 StartCoroutine(getItem());
                 ItemUIScroll.SetBool("Scroll", true); // animation 
+                audioSource.PlayOneShot(_spin, 0.3f);
             }
         }
 
         if (other.gameObject.tag == "EndPoint")
         {
-            /*
-             GameEnd Funciton in here
-             */        
+            _EndGamePanel.SetActive(true);
+            _scoreEnd = _score;
+            _ScoreEndText.text = "Your Score: " + _scoreEnd.ToString();
+            checkEnd = true;
+
+            if(checkEnd == true)
+            {
+                if(SecCount <= 9)
+                {
+                    _sec.GetComponent<TMP_Text>().text = "0" + SecCount + ":";
+                }
+                else
+                {
+                    _sec.GetComponent<TMP_Text>().text = "" + SecCount + ":";
+                }
+
+                if (MinCount <= 9)
+                {
+                    _min.GetComponent<TMP_Text>().text = "0" + MinCount + ":";
+                }
+                else
+                {
+                    _min.GetComponent<TMP_Text>().text = "" + MinCount + ":";
+                }
+
+                _mili.GetComponent<TMP_Text>().text = "" + Mathf.Round(MiliCount);
+            }
         }
 
         if (other.gameObject.tag == "Coin")
@@ -59,8 +151,8 @@ public class Player : MonoBehaviour
             _score += _addScore;
             _ScoreText.text = "Score: " + _score.ToString();
         }
-
     }
+
     private void OnTriggerStay(Collider other) // get hit when it stay
     {
         if (other.gameObject.tag == "ItemBox" && gethit == false)
@@ -86,6 +178,7 @@ public class Player : MonoBehaviour
             ItemUIScroll.SetBool("Scroll", false);
             yourSprite.GetComponent<Image>().color = new Color(255,255,255);
             hasItem = true;
+            audioSource.Stop();
         }
     }
 
@@ -111,15 +204,17 @@ public class Player : MonoBehaviour
             hasItem = false;
             yourSprite.GetComponent<Image>().color = new Color(0, 0, 0);
             yourSprite.GetComponent<Image>().sprite = null;
-            if (index == 0) {
+
+            if (index == 0)
+            {
                 Vector3 position = new Vector3(transform.position.x, transform.position.y + 0.4f, transform.position.z);
                 Instantiate(IceCream, position, transform.rotation);
             }
             
-            if (index ==1 ) {
+            if (index == 1)
+            {
                 Vector3 position = new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z);
-                Instantiate(ItemRocket, position, transform.rotation);
-                
+                Instantiate(ItemRocket, position, transform.rotation);                
             }
         }
     }
--- FinalProject/Assets/Script/UiFolCamera.cs
+++ FinalProject/Assets/Script/UiFolCamera.cs
@@ -5,18 +5,16 @@ using NRKernal;
 
 public class UiFolCamera : MonoBehaviour
 {
-    private Transform cameraCenter {
-        get {
+    private Transform cameraCenter
+    {
+        get
+        {
             return NRInput.CameraCenter;
         }
     }
-    Vector3 position;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
+    Vector3 position;
+
     void Update()
     {
         position = cameraCenter.position + cameraCenter.forward* 2f;
