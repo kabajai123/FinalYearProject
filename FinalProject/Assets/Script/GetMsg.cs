0805ab01c013631945b9281d3c8409b733f1fa7e
HappyGrape123 <103878210+HappyGrape123@users.noreply.github.com>
2023-07-07 00:09:26 +0800

    a
    fk

--- FinalProject/Assets/Script/GetMsg.cs
+++ FinalProject/Assets/Script/GetMsg.cs
@@ -19,7 +19,7 @@ public class GetMsg : MonoBehaviour
     private string[] Cut;
     void Local(Pos _msg)
     {
-
+        
         Cut = _msg.msg.Split("\n");
         string[] x = Cut[Cut.Length - 8].Split(" ");    //position x,y,z
         string[] y = Cut[Cut.Length - 7].Split(" ");
@@ -31,8 +31,9 @@ public class GetMsg : MonoBehaviour
         if (new Vector3(float.Parse(x[x.Length - 1]), float.Parse(y[y.Length - 1]), float.Parse(z[z.Length - 1])) != new Vector3(0, 0, 0))
         {
             //float.Parse(y[y.Length - 1]) * 100+500
-            CarCamera.transform.position = new Vector3(float.Parse(x[x.Length - 1]) * 100+500, 500, float.Parse(z[z.Length - 1]) * 100+500);
-            CarCamera.transform.rotation = new Quaternion(float.Parse(rx[rx.Length - 1]), float.Parse(ry[ry.Length - 1]), float.Parse(rz[rz.Length - 1]), float.Parse(rw[rw.Length - 1]));
+            CarCamera.transform.position = new Vector3(float.Parse(y[y.Length - 1])*100*-1+500  , 500, float.Parse(x[x.Length - 1])*100+500 );
+            
+            CarCamera.transform.rotation = new Quaternion(float.Parse(ry[ry.Length - 1]), float.Parse(rz[rz.Length - 1])*-1 , float.Parse(rx[rx.Length - 1]) , float.Parse(rw[rw.Length - 1]));
         }
         // This function is for moving the camera in real time
     }
