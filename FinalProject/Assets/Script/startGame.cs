560e3671650900be900383725f81d14c53195c85
kabajai123 <103850170+kabajai123@users.noreply.github.com>
2023-07-14 00:19:13 +0800

    updateRoomScene

---
+++ FinalProject/Assets/Script/startGame.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startGame : MonoBehaviour
+{
+    [SerializeField]
+    bool isRelease = false;
+
+    [SerializeField]
+    bool isSelected = false;
+
+    public NetworkSend _command;
+
+    public void Update()
+    {
+        if(isRelease == false && isSelected == false)
+        {
+            _command.Send("");
+        }
+    }
+
+    public void gameStart()
+    {
+        isSelected = true;
+    }
+
+    public void checkstart()
+    {
+        isRelease = true;
+        if (isSelected == true && isRelease == true)
+        {
+            _command.Send("0");
+        }
+        else
+        {
+            _command.Send("");
+        }
+    }
+
+    public void Exitgame()
+    {
+        Application.Quit();
+    }
+}
